d952f0bc7cbf131b42bfb2d9ffb45fa8ed9cc076
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-21 20:45:30 -0400

    scraper tests passing

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,15 +26,15 @@ class Scraper # scrapes a webpage
     
     urls = social.css('a').map { |a| a['href'] }
     ['twitter','linkedin','github'].each { |network|
-      student[network.to_sym] = urls.find {|a| a.include?(network)}
+       url = urls.find {|a| a.include?(network)}
+       urls.delete(url)
+       student[network.to_sym] = url if url
     }
-    binding.pry
-    student[:blog] = urls.last
 
+    student[:blog] = urls.first if urls.first
     student[:profile_quote] = doc.css('.profile-quote').text
     student[:bio] = doc.css('.description-holder p').text
     # binding.pry
-
     student
   end
 
