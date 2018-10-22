58517a031403ee835d51fbb7366278a532a1f13a
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-21 20:03:08 -0400

    scrape_index_page test passing

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,20 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
-class Scraper
+class Scraper # scrapes a webpage
 
   def self.scrape_index_page(index_url)
-    
+    html = open('./fixtures/student-site/index.html')
+    doc = Nokogiri::HTML(html)
+    students_html = doc.css '.student-card'  
+    students = students_html.map { |info|
+      student = {
+        name: info.css('.student-name').text,
+        location: info.css('.student-location').text,
+        profile_url: info.css('a').first['href']
+      }      
+    }
   end
 
   def self.scrape_profile_page(profile_url)
