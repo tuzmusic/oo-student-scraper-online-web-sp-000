655656cb52067da60dbb6767b64bb0f72d81ba7f
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-21 20:39:50 -0400

    All student props scraped into hash except blog.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 class Scraper # scrapes a webpage
 
   def self.scrape_index_page(index_url)
-    html = open('./fixtures/student-site/index.html')
+    html = open(index_url)
     doc = Nokogiri::HTML(html)
     students_html = doc.css '.student-card'  
     students = students_html.map { |info|
@@ -18,8 +18,26 @@ class Scraper # scrapes a webpage
   end
 
   def self.scrape_profile_page(profile_url)
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    social = doc.css('.social-icon-container')
     
+    student = {}
+    
+    urls = social.css('a').map { |a| a['href'] }
+    ['twitter','linkedin','github'].each { |network|
+      student[network.to_sym] = urls.find {|a| a.include?(network)}
+    }
+    binding.pry
+    student[:blog] = urls.last
+
+    student[:profile_quote] = doc.css('.profile-quote').text
+    student[:bio] = doc.css('.description-holder p').text
+    # binding.pry
+
+    student
   end
+
 
 end
 
